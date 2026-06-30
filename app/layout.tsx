d0cdea684d655ca06a372a5d0bf38191ad58cad6
YEO Sugyeong <ssyeo1204@gmail.com>
2026-06-30 15:31:37 +0900

    ✨ feat: 로고 아이콘 추가 및 메타데이터(파비콘, 타이틀) 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,22 @@
+import type { Metadata } from 'next';
+import { Archivo_Black } from 'next/font/google';
+import React from 'react';
 import Footer from '@/components/ui/Footer';
 import Header from '@/components/ui/Header';
 import '@/styles/globals.css';
-import { Archivo_Black } from 'next/font/google';
-import React from 'react';
 
 const archivoBlack = Archivo_Black({
   subsets: ['latin'],
   weight: ['400'],
   variable: '--font-archivo-black',
 });
+
+export const metadata: Metadata = {
+  title: '여수경 | 포트폴리오',
+  icons: {
+    icon: '/icons/logo-lime.svg',
+  },
+};
 
 export default function RootLayout({
   children,
