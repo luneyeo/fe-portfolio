715b3775579286b787804880326eb6d182fa1268
YEO Sugyeong <ssyeo1204@gmail.com>
2026-07-16 16:55:50 +0900

    ♻️ Refactor: HeroSection에서 CoverLetter 컴포넌트로 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import AnimateOnView from '@/components/common/AnimateOnView';
+import CoverLetter from '@/components/sections/CoverLetter';
 import ExperienceSection from '@/components/sections/ExperienceSection';
-import HeroSection from '@/components/sections/HeroSection';
 import ProjectsSection from '@/components/sections/ProjectsSection';
 import SkillsSection from '@/components/sections/SkillsSection';
 
 export default function Home() {
   return (
     <>
-      <HeroSection />
+      <CoverLetter />
       <div className="relative z-10 flex flex-col gap-27 rounded-t-4xl bg-white pt-20 pb-30 shadow-[0_8px_40px_rgba(0,0,0,0.6)] md:gap-40 md:pt-30 lg:pt-40">
         <AnimateOnView>
           <SkillsSection />
